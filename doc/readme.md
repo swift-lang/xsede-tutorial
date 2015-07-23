6b5f0deb755dca3cf46760b94645ec4abb1fef05
Yadu Babuji <yadunandb@communicado.ci.uchicago.edu>
2015-07-23 11:18:29 -0500

    Updates

---
+++ doc/readme.md
@@ -0,0 +1,13 @@
+Mike has an asciidoc and source-highlight build in communicado.
+The setup adds these to the path.
+
+To build
+
+----
+source setup.sh
+./build_docs.sh
+----
+
+
+The google diagrams are here:
+https://drive.google.com/drive/u/0/folders/0B93kSCcfC7Ihck9uRTVDX3VFemM
--- doc/setup.sh
+++ doc/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-
-export PATH=~davidk/asciidoc-8.6.4/:~davidk/source-highlight/bin/:$PATH
+export PATH=~wilde/asciidoc/asciidoc-8.6.4.install/bin/:~wilde/source-highlight.3.1.8.install/bin/:$PATH
+#export PATH=~davidk/asciidoc-8.6.4/:~davidk/source-highlight/bin/:$PATH

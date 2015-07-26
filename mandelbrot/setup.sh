c9582170bbd36b38b58d51049e5349eb193202ec
Yadu Nand Babuji <yadudoc1729@gmail.com>
2015-07-26 16:32:00 -0500

    Updates to mandelbrot. Runs correctly on stampede with 2 nodes, and single node on blacklight

--- mandelbrot/setup.sh
+++ mandelbrot/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PWD/bin:$PATH
+
 if [[ "$HOSTNAME" == *stampede* ]]
 then
     echo "XSEDE/Stampede"
@@ -16,7 +18,8 @@ then
 elif [[ "$HOSTNAME" == *workflow.iu.xsede.org* ]]
 then
     export STAMPEDE_HOME=$(ssh stampede.tacc.xsede.org pwd)
-    export GORDON_HOME=$(ssh gordon.sdsc.xsede.org pwd)
+    #export GORDON_HOME=$(ssh gordon.sdsc.xsede.org pwd)
+    export BLACKLIGHT_HOME=$(ssh blacklight.psc.xsede.org pwd)
 else
     SWIFT=$PWD/swift-trunk/bin
     export PATH=$SWIFT:$PWD/bin:$PATH

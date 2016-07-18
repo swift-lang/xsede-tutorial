e6e1148bdf15b98313b7b094f13b0f2d9b639636
account train01 <xdtr1@comet-ln2.sdsc.edu>
2016-07-18 13:25:54 -0700

    Determine pathname of MPI executable by dynamically finding the current working dir.

--- part07/mpi_hello.swift
+++ part07/mpi_hello.swift
@@ -1,17 +1,16 @@
 type file;
 
-global string mpiapp = "/oasis/scratch/comet/xdtr1/temp_project/swift/swift-tutorial/mpi/mpi_hello";
+string curdir = java("java.lang.System","getProperty","user.dir");
 
 app (file out, file err) mpi_hello (int time, int nproc)
 {
     ibrun "-np" nproc mpiapp time stderr=filename(err) stdout=filename(out);
 }
 
-int nsim   = toInt(arg("nsim",   "10"));
-int time   = toInt(arg("time",   "1"));
-int nproc  = toInt(arg("nproc",  "4"));
-
-file mpiapp <"mpi_hello">;
+int    nsim   = toInt(arg("nsim",   "10"));
+int    time   = toInt(arg("time",   "1"));
+int    nproc  = toInt(arg("nproc",  "4"));
+global string mpiapp = arg("mpiapp", curdir+"/mpi_hello");
 
 foreach i in [0:nsim-1] {
   file mpiout <single_file_mapper; file=strcat("output/mpi_",i,".out")>;

fbb520b1b93bbc89e5a46a66517715fb218a2d55
account train01 <xdtr1@comet-ln3.sdsc.edu>
2016-07-18 08:37:45 -0700

    Change app name to file accessible to all participants.

--- mpi/mpi_hello.swift
+++ mpi/mpi_hello.swift
@@ -1,8 +1,10 @@
 type file;
 
-app (file out, file err) mpi_hello (file mpiapp, int time, int nproc)
+global string mpiapp = "/oasis/scratch/comet/xdtr1/temp_project/swift/swift-tutorial/mpi/mpi_hello";
+
+app (file out, file err) mpi_hello (int time, int nproc)
 {
-    ibrun "-np" nproc "/home/xdtr1/swift-tutorial/mpi/mpi_hello" time stderr=filename(err) stdout=filename(out);
+    ibrun "-np" nproc mpiapp time stderr=filename(err) stdout=filename(out);
 }
 
 int nsim   = toInt(arg("nsim",   "10"));
@@ -14,5 +16,5 @@ file mpiapp <"mpi_hello">;
 foreach i in [0:nsim-1] {
   file mpiout <single_file_mapper; file=strcat("output/mpi_",i,".out")>;
   file mpierr <single_file_mapper; file=strcat("output/mpi_",i,".err")>;
-  (mpiout, mpierr) = mpi_hello(mpiapp, time, nproc);
+  (mpiout, mpierr) = mpi_hello(time, nproc);
 }
